dcdadc431cfacc324acc3c97886835d0bdaf8c1e
lucas <lucasriibeiro23@gmail.com>
2023-09-15 19:31:07 -0300

    requisito 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import './App.css';
 import Table from './components/Table';
 import SearchInput from './components/SearchInput';
-import { SearchContextProvider } from './context/SearchContext';
 import SelectFilters from './components/SelectFilters';
-import { FilterProvider } from './context/FilterContext';
 import { PlanetContextProvider } from './context/PlanetContext';
 
 function App() {
   return (
     <PlanetContextProvider>
-      <FilterProvider>
-        <SearchContextProvider>
-          <SearchInput />
-          <SelectFilters />
-          <Table />
-        </SearchContextProvider>
-      </FilterProvider>
+      <SearchInput />
+      <SelectFilters />
+      <Table />
     </PlanetContextProvider>
   );
 }
